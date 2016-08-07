d22b685d7265bc40a7c18b4b160a2b86b28530c4
Chad Elder <chud92@msn.com>
2016-08-07 09:45:14 -0400

    Another Commit

--- server/controllers/meetups-controller.js
+++ server/controllers/meetups-controller.js
@@ -2,13 +2,13 @@ var Meetup = require('../models/meetup');
 
 module.exports.create = function (req, res) {
   var meetup = new Meetup(req.body);
-  meetup.save(function (err, result) {
+  meetup.save(function (err, order) {
     res.json(result);
   });
 }
 
 module.exports.list = function (req, res) {
-  Meetup.find({}, function (err, results) {
+  Meetup.find({}, function (err, order) {
     res.json(results);
   });
 }
